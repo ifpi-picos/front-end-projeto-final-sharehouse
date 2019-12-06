13fcb1fcca52a0f4338916bf13a4e874170991e1
Lucas Paz <eu@lucaspaz.com>
2019-12-06 13:38:41 -0300

    Novo login, novo registro

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ---
 #### Links
 * [Página Inicial](http://sharehouse.surge.sh/index.html)
-* [Login](http://sharehouse.surge.sh/login.html)
-* [Criar uma conta](http://sharehouse.surge.sh/register.html)
+* [Login](http://sharehouse.surge.sh/index.html#login) / [Login 2](http://sharehouse.surge.sh/login-2.html)
+* [Criar uma conta](http://sharehouse.surge.sh/register.html) / [Criar uma conta 2](http://sharehouse.surge.sh/register-2.html)
 * [Configurações](http://sharehouse.surge.sh/settings.html)
 * [Anunciar propriedade](http://sharehouse.surge.sh/post.html)
---
+++ app/boot.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const router = express();
+
+router.use("/assets", express.static(path.resolve(__dirname + '/../assets')));
+
+router.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/../index.html'));
+});
+
+router.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/../register.html'));
+});
+
+router.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/../post.html'));
+});
+
+router.get('/settings', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/../settings.html'));
+});
+
+router.listen(3000, () => console.log('Listening: http://127.0.0.1:3000'));
